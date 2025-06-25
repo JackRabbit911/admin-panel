cd8009d8dc16d649ef19b81cd148467138035af9
alx <world.zay@gmail.com>
2025-06-25 15:03:42 +0300

    [build] Create store/login/index.ts

---
+++ src/store/login/index.ts
@@ -0,0 +1,40 @@
+import { createEffect, createEvent, createStore, sample } from "effector"
+import { apiTryLogin } from "api/auth"
+import type { LoginError, LoginPayload } from "./types"
+import { currentUserRecived } from "store/currentUser"
+import { engageTokenFx } from "store/token"
+
+export const emailChanged = createEvent<string>()
+export const passwordChanged = createEvent<string>()
+export const tryLoginClicked = createEvent()
+const errorRecived = createEvent<LoginError | undefined>()
+
+export const tryLoginFx = createEffect(apiTryLogin)
+
+export const $loginPayload = createStore<LoginPayload>({ email: '', password: '' })
+    .on(emailChanged, (store, email) => ({...store, email: email}))
+    .on(passwordChanged, (store, password) => ({...store, password: password}))
+
+export const $errorLogin = createStore<LoginError | null>(null)
+    .on(errorRecived, (store, error) => !error ? store : ({...store, ...error}))
+
+sample({
+    clock: tryLoginClicked,
+    source: $loginPayload,
+    target: tryLoginFx,
+})
+
+sample({
+    clock: tryLoginFx.doneData,
+    filter: (response) => response.data.success === true,
+    fn: (response) => response.data.result,
+    target: [currentUserRecived, engageTokenFx],
+})
+
+sample({
+    clock: tryLoginFx.doneData,
+    filter: (response) => response.data.success === false && Boolean(response.data.error),
+    fn: (response) => response.data.error,
+    target: errorRecived,
+})
+
