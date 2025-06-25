b1378200b7bec3cabb255694e73c8b3a5aca08a1
alx <world.zay@gmail.com>
2025-06-25 15:05:31 +0300

    [build] Create store currentUser and token

---
+++ src/store/currentUser/index.ts
@@ -0,0 +1,8 @@
+import { createEvent, createStore } from "effector";
+import type { CurrentUser } from "./types";
+import type { LoginSuccess } from "store/login/types";
+
+export const currentUserRecived = createEvent<LoginSuccess>()
+
+export const $currentUser = createStore<CurrentUser | null>(null)
+    .on(currentUserRecived, (_, { user }) => user)
