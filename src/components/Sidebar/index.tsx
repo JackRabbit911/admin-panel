5673addef9cd5b754501467581ec77e2ccdbd406
alx <world.zay@gmail.com>
2025-07-07 16:55:25 +0300

    #10 Update Drawer and Sidebar components

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -2,13 +2,20 @@ import { NavLink } from "react-router";
 import { sideItems } from "./constants";
 import Submenu from "./Submenu";
 
-const Sidebar = () => {
+type Props = {
+  onClose: () => void;
+}
+
+const Sidebar = ({onClose}: Props) => {
   return (
     <ul className="menu bg-base-200 text-base-content min-h-full w-full p-2">
       {sideItems.map(
         (item, key) => !item?.sub ? (
           <li key={key}>
-            <NavLink to={item.href as string}>
+            <NavLink
+              to={item.href as string}
+              onClick={onClose}
+            >
               {item.title}
             </NavLink>
           </li>
