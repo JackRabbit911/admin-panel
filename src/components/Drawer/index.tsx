7adad066483dc651da3358157e1d293e8f533e52
alx <world.zay@gmail.com>
2025-07-07 12:54:45 +0300

    #11 Update Draver and Sidebar components

--- src/components/Drawer/index.tsx
+++ src/components/Drawer/index.tsx
@@ -1,5 +1,7 @@
+import { NavLink } from "react-router";
 import Navbar from "../Navbar";
 import Sidebar from "../Sidebar";
+import Router from "components/Router";
 
 const Drawer = () => {
   return (
@@ -8,14 +10,19 @@ const Drawer = () => {
         <input id="my-drawer" type="checkbox" className="drawer-toggle" />
         <div className="drawer-content">
           <Navbar />
+          <div className="md:p-3">
+            <Router />
+          </div>
         </div>
         <div className="drawer-side">
           <label htmlFor="my-drawer" aria-label="close sidebar" className="drawer-overlay"></label>
           <aside className="bg-base-200 text-base-content min-h-full w-64 xl:w-80">
             <div className="flex justify-center navbar bg-lime-200 sticky top-0 shadow-sm z-40 w-full py-0">
-              <h2 className="text-center text-2xl font-medium text-violet-800">
-                Admin panel
-              </h2>
+              <NavLink to='/'>
+                <h2 className="text-center text-2xl font-medium text-violet-800">
+                  Admin panel
+                </h2>
+              </NavLink>
             </div>
             <Sidebar />
           </aside>
