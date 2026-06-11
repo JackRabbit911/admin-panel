1fad057da58a4e18ee102a848f525ac9406b72ab
alx <world.zay@gmail.com>
2026-06-11 14:12:51 +0300

    #1 Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen gap-4 bg-base-200 text-base-content">
+      <h1 className="text-4xl font-extrabold text-primary">
+        Vite + Tailwind + daisyUI
+      </h1>
+      <p className="text-lg">
+        Your TypeScript React template is ready!
+      </p>
+      <button className="btn btn-primary">daisyUI Button</button>
+    </div>
+  )
+}
+
+export default App
