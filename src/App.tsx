ab9bd83c874011b7eb004aa1571bd23ad763e472
alx <world.zay@gmail.com>
2026-06-12 14:35:22 +0300

    #3 Delete test lines and apply Layout component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,8 @@
+import Layout from "./Layout"
+
 function App() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen gap-4 bg-base-200 text-base-content">
-      <h1 className="text-4xl font-extrabold text-primary">
-        Vite + Tailwind + daisyUI
-      </h1>
-      <p className="text-lg">
-        Your TypeScript React template is ready!
-      </p>
-      <button className="btn btn-primary">daisyUI Button</button>
-    </div>
+    <Layout />
   )
 }
 
