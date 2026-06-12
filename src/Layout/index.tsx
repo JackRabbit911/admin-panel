bbf92695b776909d1a52712458ea3a600f83cc0b
alx <world.zay@gmail.com>
2026-06-12 14:38:12 +0300

    #3 Create Layout components

---
+++ src/Layout/index.tsx
@@ -0,0 +1,48 @@
+import { useState } from "react"
+import Navbar from "./Navbar"
+import { NavLink } from "react-router"
+import Sidebar from "../Sidebar"
+import Router from "./Router"
+
+const Layout = () => {
+  const [isShown, setIsShown] = useState(false)
+
+  const onClose = () => {
+    setIsShown(false)
+  }
+
+  return (
+    <div className="drawer md:drawer-open">
+      <input
+        id="my-drawer"
+        type="checkbox"
+        checked={isShown}
+        onChange={() => {
+          setIsShown(!isShown)
+        }}
+        className="drawer-toggle"
+      />
+      <div className="drawer-content">
+        <Navbar />
+        <div className="p-3">
+          <Router />
+        </div>
+      </div>
+      <div className="drawer-side">
+        <label htmlFor="my-drawer" aria-label="close sidebar" className="drawer-overlay"></label>
+        <aside className="bg-base-200 text-base-content min-h-full w-64 xl:w-80">
+          <nav className="navbar shadow-sm flex justify-center sticky top-0 bg-lime-200 h-16 z-40">
+            <NavLink onClick={onClose} to='/'>
+              <h2 className="text-center text-2xl font-medium text-violet-800">
+                Admin panel
+              </h2>
+            </NavLink>
+          </nav>
+          <Sidebar onClose={onClose} />
+        </aside>
+      </div>
+    </div>
+  )
+}
+
+export default Layout
