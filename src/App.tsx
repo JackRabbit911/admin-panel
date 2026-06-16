691c85a8cdfd624e6d53739aa91dd7fb600fdcba
alx <world.zay@gmail.com>
2026-06-16 13:24:42 +0300

    #5 Apply username store and TranslateProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
+import { useLocation } from "react-router"
+
 import Auth from "./Auth"
 import AdminPanel from "./AdminPanel"
+import { useAppSelector } from "./shared/store/hooks"
+import TranslateProvider from "./shared/i18n/TranslateProvider"
 
 function App() {
-  const user = null
+  const location = useLocation()
+  const username = useAppSelector((state) => state.username.name)
 
-  return user ? <AdminPanel /> : <Auth />
+  return (
+    <TranslateProvider deps={[location]}>
+      {username ? <AdminPanel /> : <Auth />}
+    </TranslateProvider>
+  )
 }
 
 export default App
