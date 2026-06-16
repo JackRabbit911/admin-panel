8bf4e749dda0050d4a04ae13c2cc95e156430241
alx <world.zay@gmail.com>
2026-06-16 13:59:44 +0300

    #5 Add username reducer

--- src/shared/store/index.ts
+++ src/shared/store/index.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit"
 
 import { api } from "../api"
+import usernameReducer from "./username"
 
 export const store = configureStore({
     reducer: {
+        username: usernameReducer,
         [api.reducerPath]: api.reducer,
     },
     middleware: (getDefaultMiddleware) =>
