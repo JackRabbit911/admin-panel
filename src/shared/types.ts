4437929606aa07a98a5bbd1c5600b1c66f867f83
alx <world.zay@gmail.com>
2026-06-20 15:42:48 +0300

    #5 Add User type

--- src/shared/types.ts
+++ src/shared/types.ts
@@ -2,3 +2,9 @@ import type { SerializedError } from "@reduxjs/toolkit";
 import type { FetchBaseQueryError } from "@reduxjs/toolkit/query";
 
 export type ServerError = FetchBaseQueryError | SerializedError | undefined;
+
+export type User = {
+    id?: number;
+    name?: string;
+    role?: number;
+}
