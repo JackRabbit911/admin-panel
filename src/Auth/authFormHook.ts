cf25f3f655f2be9217d452a3d9af5cc7c27c6a69
alx <world.zay@gmail.com>
2026-06-24 17:56:30 +0300

    #5 Apply seCredentials reducer

--- src/Auth/authFormHook.ts
+++ src/Auth/authFormHook.ts
@@ -1,46 +1,54 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm, type SubmitHandler } from "react-hook-form";
 
-import { useAuthMutation } from "../shared/api";
 import { isObjectEmpty } from "../shared/utils";
-import { setUser } from "../shared/store/authSlice";
+import { usePostMutation } from "../shared/api";
 import { useAppDispatch } from "../shared/store/hooks";
+import { setCredentials } from "../shared/store/authSlice";
 import { useFormServerError } from "../shared/hooks/formServerError";
 import { authData, type AuthData } from "./schema";
 import type { ServerError } from "../shared/types";
 
 export const useAuthForm = () => {
-    const [auth, { isLoading, isError, error }] = useAuthMutation()
-      const { handleServerError } = useFormServerError<AuthData>();
-      const dispatch = useAppDispatch()
-      const err = error as ServerError
-      const responseStatus = { isLoading, isError, err }
-    
-      const methods = useForm({
-        resolver: zodResolver(authData),
-        reValidateMode: "onChange",
-        defaultValues: {
-          email: '',
-          password: '',
-          remember: true,
-        },
-      })
-    
-      const onSubmit: SubmitHandler<AuthData> = async (formData) => {
-        try {
-          const data = await auth(formData).unwrap()
-          dispatch(setUser(data?.result?.user))
-        } catch (err) {
-          const isHandled = handleServerError(err, methods.setError);
-          if (!isHandled) {
-            console.error('Глобальная ошибка сервера (не 422):', err);
-          }
-        }
+  const [auth, { isLoading, isError, error }] = usePostMutation()
+  const { handleServerError } = useFormServerError<AuthData>();
+  const dispatch = useAppDispatch()
+  const err = error as ServerError
+  const responseStatus = { isLoading, isError, err }
+
+  const methods = useForm({
+    resolver: zodResolver(authData),
+    reValidateMode: "onChange",
+    defaultValues: {
+      email: '',
+      password: '',
+      remember: true,
+    },
+  })
+
+  const onSubmit: SubmitHandler<AuthData> = async (formData) => {
+    try {
+      const arg = {
+        url: '/auth/login',
+        body: formData,
       }
-    
-      const disabled = !isObjectEmpty(methods.formState.errors) ||
-        methods.watch('email') === '' ||
-        methods.watch('password') === ''
-    
-    return { methods, onSubmit, disabled, responseStatus }
+
+      const data = await auth(arg).unwrap()
+      const { refresh, bearer, user } = data?.result
+
+      dispatch(setCredentials({ user, refresh, bearer }))
+
+    } catch (err) {
+      const isHandled = handleServerError(err, methods.setError);
+      if (!isHandled) {
+        console.error('Глобальная ошибка сервера (не 422):', err);
+      }
+    }
+  }
+
+  const disabled = !isObjectEmpty(methods.formState.errors) ||
+    methods.watch('email') === '' ||
+    methods.watch('password') === ''
+
+  return { methods, onSubmit, disabled, responseStatus }
 }
