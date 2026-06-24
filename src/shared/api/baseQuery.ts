184873f0c86c32f0dabb27b55aaf2139124b63a4
alx <world.zay@gmail.com>
2026-06-24 18:06:46 +0300

    #5 Create myBaseQuery insead of axiosBaseQuery

--- src/shared/api/baseQuery.ts
+++ src/shared/api/baseQuery.ts
@@ -1,32 +1,62 @@
-import type { AxiosError, AxiosRequestConfig } from 'axios';
-import type { BaseQueryFn } from '@reduxjs/toolkit/query/react';
+import { fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import type { BaseQueryFn, FetchArgs, FetchBaseQueryError } from '@reduxjs/toolkit/query/react';
 
-import ajax from './ajax';
-import { logout } from '../store/authSlice';
+import { logout, setTokens } from '../store/authSlice';
+import type { RootState } from '../store';
+import type { ApiResponse, AuthTokens } from '../types';
 
-export const axiosBaseQuery =
-  (): BaseQueryFn<
-    {
-      url: string;
-      method?: AxiosRequestConfig['method'];
-      data?: AxiosRequestConfig['data'];
-      params?: AxiosRequestConfig['params'];
-    }
-  > =>
-    async ({ url, method = 'GET', data, params }, api) => {
-      try {
-        const result = await ajax({ url, method, data, params });
-        return { data: result.data };
-      } catch (error) {
-        const axiosError = error as AxiosError
-        const status = axiosError.status || 500
+const { protocol, hostname } = window.location
+export const host = `${protocol}//${hostname}`
 
-        if (status === 401) {
-          api.dispatch(logout())
+const refreshApi = {
+    url: '/auth/refresh',
+    method: 'PATCH',
+    body: {},
+}
+
+const baseQuery = fetchBaseQuery({
+    baseUrl: `${host}/api/adm`,
+    prepareHeaders: (headers, { getState }) => {
+        const { bearer } = (getState() as RootState).auth
+
+        if (bearer) {
+            headers.set('authorization', `Bearer ${bearer}`);
         }
-        
-        return {
-          error: axiosError, // Здесь будет объект с ошибками, выброшенный из interceptor
-        };
-      }
-    };
+
+        return headers;
+    },
+})
+
+export const myBaseQuery = (): BaseQueryFn<
+    FetchArgs,
+    unknown,
+    FetchBaseQueryError
+> => async (args, api, extraOptions) => {
+    let result = await baseQuery(args, api, extraOptions)
+
+    // ПЕРЕХВАТ ОШИБКИ 401 (Сессия устарела)
+    if (result.error && result.error.status === 401) {
+        const state = api.getState() as RootState
+        const refresh = state.auth.refresh
+
+        refreshApi.body = { refresh }
+        const response = await baseQuery(refreshApi, api, extraOptions);
+        const data = response?.data as ApiResponse<AuthTokens>
+
+        if (data?.success && data?.result) {
+            console.log(data?.result)
+            api.dispatch(setTokens(data.result))
+            result = await baseQuery(args, api, extraOptions);
+        } else {
+            console.log('logout')
+            api.dispatch(logout())
+        }
+    }
+
+    // ПЕРЕХВАТ ОШИБКИ 422 (Ошибки валидации бэкенда)
+    if (result.error && result.error.status === 422) {
+        console.warn('Ошибка валидации данных (422):', result.error.data);
+    }
+
+    return result;
+};
