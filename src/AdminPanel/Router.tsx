dbac00e0390ab36ef916b40dbcc697335c2138e5
alx <world.zay@gmail.com>
2026-07-20 18:01:02 +0300

    #18 Add some routes

--- src/AdminPanel/Router.tsx
+++ src/AdminPanel/Router.tsx
@@ -4,7 +4,17 @@ import Home from "../Home"
 const Router = () => {
   return (
     <Routes>
-      <Route index element={<Home />} />
+      <Route path='' element={<Home />} />
+      <Route path='pages' element={'Pages'} />
+      <Route path='users' element={'Users'} />
+      <Route path='tests' element={'Tests'} />
+      <Route path='deploy' element={'Deploy'} />
+      <Route path='seo' element={'SEO'} />
+      <Route path='burime'>
+        <Route path='works' element={'Works'} />
+        <Route path='authors' element={'Authors'} />
+      </Route>
+      <Route path='*' element={'404'} />
     </Routes>
   )
 }
