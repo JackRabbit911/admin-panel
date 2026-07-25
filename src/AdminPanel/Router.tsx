8619e87773d54af3b212c516c73976fe5d0778ec
alx <world.zay@gmail.com>
2026-07-25 11:57:23 +0300

    #24 Add user/:id route and update imports

--- src/AdminPanel/Router.tsx
+++ src/AdminPanel/Router.tsx
@@ -1,15 +1,17 @@
 import { Route, Routes } from "react-router"
 import Home from "Home"
-import Users from "Pages/Users"
+import User from "Pages/User"
 import Pages from "Pages/Pages"
 import Tests from "Pages/Tests"
+import Users from "Pages/Users"
 
 const Router = () => {
   return (
     <Routes>
       <Route path='' element={<Home />} />
       <Route path='pages' element={<Pages />} />
-      <Route path='users/:id?' element={<Users />} />
+      <Route path='users' element={<Users />} />
+      <Route path='user/:id' element={<User />} />
       <Route path='tests' element={<Tests />} />
       <Route path='deploy' element={'Deploy'} />
       <Route path='seo' element={'SEO'} />
