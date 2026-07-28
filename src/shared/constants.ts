359905d2ca86a8127502eb89655a1392889cbdfb
alx <world.zay@gmail.com>
2026-07-28 16:40:41 +0300

    #27 Add adin permissions constants

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -1,3 +1,4 @@
+//endpoints urls
 export const authUrl = '/auth/auth'
 export const logoutUrl = '/auth/logout'
 export const refreshUrl = '/auth/refresh'
@@ -5,4 +6,15 @@ export const getSidebarUrl = '/home/sidebar'
 export const getDashboardUrl = '/home/dashboard'
 export const getPagesUrl = '/home/pages'
 export const getUsersUrl = '/users'
+
+//admin permissions
+export const P_TRANSLATE = 1 << 0
+export const P_SEO = 1 << 1
+export const P_BURIME = 1 << 2
+export const P_CONTENT = 1 << 3
+export const P_DEVELOP = 1 << 4
+export const P_USERS = 1 << 5
+export const P_ADMIN = 1 << 6
+export const P_COMMERCE = 1 << 7
+export const P_ROOT = 255
  
