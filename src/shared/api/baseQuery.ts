cb2a15fd909499b81602d912407d5355718952af
alx <world.zay@gmail.com>
2026-08-05 11:45:42 +0300

    #35 Add error status dispatch

--- src/shared/api/baseQuery.ts
+++ src/shared/api/baseQuery.ts
@@ -7,6 +7,7 @@ import { logout, setToken } from 'shared/store/tokenSlice'
 
 import type { RootState } from 'shared/store'
 import type { ApiResponse } from 'shared/types'
+import { setStatus } from 'shared/store/statusSlice'
 
 const { protocol, hostname } = window.location
 export const host = `${protocol}//${hostname}`
@@ -22,7 +23,7 @@ const mutex = new Mutex()
 const baseQuery = fetchBaseQuery({
     baseUrl: `/api/adm`,
     prepareHeaders: (headers, { getState }) => {
-        const { token: { bearer} } = (getState() as RootState)
+        const { token: { bearer } } = (getState() as RootState)
 
         if (bearer) {
             headers.set('authorization', `Bearer ${bearer}`)
@@ -40,41 +41,39 @@ export const myBaseQuery = (): BaseQueryFn<
     await mutex.waitForUnlock()
     let result = await baseQuery(args, api, extraOptions)
 
-    // ПЕРЕХВАТ ОШИБКИ 401 (Сессия устарела)
-    if (result.error && result.error.status === 401) {
-        const currentUrl = typeof args === 'string' ? args : args.url
-
-        if (currentUrl === authUrl || currentUrl === refreshUrl) {
-            window.location.href = `${host}/auth`
-            return result
-        }
-
-        if (!mutex.isLocked()) {
-            const release = await mutex.acquire()
-
-            try {
-                const response = await baseQuery(refreshApi, api, extraOptions)
-                const data = response?.data as ApiResponse<string>
-                
-                if (data?.success && data?.result) {
-                    api.dispatch(setToken(data.result))
-                    result = await baseQuery(args, api, extraOptions)
-                } else {
-                    api.dispatch(logout())
-                    window.location.href = `${host}/auth`
-                }
-            } finally {
-                release()
+    if (result.error) {
+        if (result.error.status === 401) {
+            const currentUrl = typeof args === 'string' ? args : args.url
+    
+            if (currentUrl === authUrl || currentUrl === refreshUrl) {
+                window.location.href = `${host}/auth`
+                return result
             }
-        } else {
-            await mutex.waitForUnlock();
-            result = await baseQuery(args, api, extraOptions)
+    
+            if (!mutex.isLocked()) {
+                const release = await mutex.acquire()
+    
+                try {
+                    const response = await baseQuery(refreshApi, api, extraOptions)
+                    const data = response?.data as ApiResponse<string>
+    
+                    if (data?.success && data?.result) {
+                        api.dispatch(setToken(data.result))
+                        result = await baseQuery(args, api, extraOptions)
+                    } else {
+                        api.dispatch(logout())
+                        window.location.href = `${host}/auth`
+                    }
+                } finally {
+                    release()
+                }
+            } else {
+                await mutex.waitForUnlock();
+                result = await baseQuery(args, api, extraOptions)
+            }
+        } else if (result.error && result.error.status !== 422) {
+            api.dispatch(setStatus(result.error.status))
         }
-    }
-
-    // ПЕРЕХВАТ ОШИБКИ 422 (Ошибки валидации бэкенда)
-    if (result.error && result.error.status === 422) {
-        console.warn('Ошибка валидации данных (422):', result.error.data)
     }
 
     return result
