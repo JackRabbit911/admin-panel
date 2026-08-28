3883d13c4cf6e96b1236df61996d4b0d193fe283
alx <world.zay@gmail.com>
2026-08-28 14:25:03 +0300

    #44 Add CONFIRM and BACKUP properties

--- src/shared/modalRegistry.ts
+++ src/shared/modalRegistry.ts
@@ -1,13 +1,14 @@
 import React from 'react';
-import type { ModalType } from './store/modalSlice'
-import BitMask from 'Pages/User/Form/BitMask';
+
 import AlertModal from 'Reused/AlertModal';
+import BitMask from 'Pages/User/Form/BitMask';
+import ConfirmModal from 'Reused/ConfirmModal';
+import BackupName from 'Pages/Tests/Form/BacupName';
+import type { ModalType } from './store/modalSlice'
 
 interface ModalConfig {
   component: React.ComponentType<any>;
-  /** Классы ширины Tailwind v4 (например: 'max-w-md', 'max-w-2xl', 'max-w-5xl') */
-  maxWidth: string;
-  /** Поведение на мобилках: '' (обычное) или 'modal-bottom sm:modal-middle' (шторка снизу) */
+  maxWidth?: string;
   responsiveStyle?: string;
 }
 
@@ -15,24 +16,19 @@ export const MODAL_REGISTRY: Record<ModalType, ModalConfig> = {
     ALERT: {
         component: AlertModal,
         maxWidth: 'max-w-sm',
+        responsiveStyle: 'modal-bottom sm:modal-middle',
     },
     BITMASK: {
         component: BitMask,
         maxWidth: 'max-w-sm',
+        responsiveStyle: 'modal-bottom sm:modal-middle',
+    },
+    CONFIRM: {
+        component: ConfirmModal,
+        maxWidth: 'max-w-sm',
+    },
+    BACKUP: {
+        component: BackupName,
+        maxWidth: 'max-w-sm',
     }
-
-//   CREATE_USER: {
-//     component: CreateUserModal,
-//     maxWidth: 'max-w-md', // Стандартный размер
-//     responsiveStyle: 'modal-bottom sm:modal-middle', // На смартфонах будет выезжать снизу
-//   },
-//   DELETE_CONFIRM: {
-//     component: DeleteConfirmModal,
-//     maxWidth: 'max-w-sm', // Компактное окно для алертов
-//   },
-//   EDIT_PROFILE: {
-//     component: EditProfileModal,
-//     maxWidth: 'max-w-2xl', // Широкое окно для детальной формы
-//     responsiveStyle: 'modal-bottom sm:modal-middle',
-//   },
 }
