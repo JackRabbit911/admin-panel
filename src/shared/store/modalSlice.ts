ebdb2ec03f0e83c4f1eccf1e10e5bf265db9f019
alx <world.zay@gmail.com>
2026-08-28 14:23:49 +0300

    #44 Add CONFIRM and BACKUP types

--- src/shared/store/modalSlice.ts
+++ src/shared/store/modalSlice.ts
@@ -1,11 +1,24 @@
 import { createSlice, type PayloadAction } from '@reduxjs/toolkit'
+import type { ActionType } from 'Pages/Tests/utils';
 import type { User } from 'Pages/Users/types';
 
-export type ModalType = 'ALERT' | 'BITMASK'
+export type ModalType = 'ALERT' | 'BITMASK' | 'CONFIRM' | 'BACKUP'
 
-export interface ModalPropsMap {
-  ALERT: { title: string; message: string; type: 'info' | 'success' | 'warning' },
-  BITMASK: { adminRole: number, user: User },
+export type ModalPropsMap = {
+  ALERT: { title?: string; message: string; type?: 'info' | 'success' | 'warning' };
+  BITMASK: { adminRole: number, user: User };
+  CONFIRM: { 
+    title?: string; 
+    message?: string;
+    onConfirm?: string;
+    actionType?: ActionType;
+    method?: string;
+    url?: string;
+    confirmText?: string;
+    cancelText?: string;
+    payload?: Record<string, unknown>;
+  };
+  BACKUP: { tables: string[] };
 }
 
 interface ModalState {
@@ -30,8 +43,8 @@ const modalSlice = createSlice({
       state: ModalState,
       action: PayloadAction<{ type: T; props: ModalPropsMap[T] }>
     ) => {
-      state.type = action.payload.type
-      state.props = action.payload.props
+      state.type = action.payload.type;
+      state.props = action.payload.props as any; 
     },
     closeModal: (state) => {
       state.type = null
@@ -43,12 +56,35 @@ const modalSlice = createSlice({
     ) => {
       if (typeof action.payload === 'boolean') {
         state.isGlobalLoading = action.payload
-        if (!action.payload) state.loadingMessage = null // Сбрасываем текст при выключении
+        if (!action.payload) state.loadingMessage = null
       } else {
         state.isGlobalLoading = action.payload.isActive;
         state.loadingMessage = action.payload.message || null
       }
     },
+  //   openModal: <T extends ModalType>(
+  //     state: ModalState,
+  //     action: PayloadAction<{ type: T; props: ModalPropsMap[T] }>
+  //   ) => {
+  //     state.type = action.payload.type
+  //     state.props = action.payload.props
+  //   },
+  //   closeModal: (state) => {
+  //     state.type = null
+  //     state.props = null
+  //   },
+  //   setGlobalLoading: (
+  //     state, 
+  //     action: PayloadAction<boolean | { isActive: boolean; message?: string }>
+  //   ) => {
+  //     if (typeof action.payload === 'boolean') {
+  //       state.isGlobalLoading = action.payload
+  //       if (!action.payload) state.loadingMessage = null // Сбрасываем текст при выключении
+  //     } else {
+  //       state.isGlobalLoading = action.payload.isActive;
+  //       state.loadingMessage = action.payload.message || null
+  //     }
+  //   },
   },
 })
 
