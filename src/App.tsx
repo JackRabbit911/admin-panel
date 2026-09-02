0d7e1153ddffef90a0e1d47fa7c8004fa62520c0
alx <world.zay@gmail.com>
2026-09-02 15:46:33 +0300

    #51 Remove logic to AdminPanel

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,10 @@
-import { useEffect } from "react"
 import AdminPanel from "AdminPanel"
-import { useGetQuery } from "shared/api"
-import { getUserByJWT } from "shared/utils"
-import { authUrl } from "shared/constants"
-import { setUser } from "shared/store/userSlice"
 import ModalContainer from "Reused/ModalContainer"
-import { setToken } from "shared/store/tokenSlice"
-import { useAppDispatch } from "shared/store/hooks"
-import Error from "Reused/Error"
 
 function App() {
-  const { data, isError, error } = useGetQuery(authUrl)
-  const dispatch = useAppDispatch()
-
-  const token = data?.result ? data.result : null
-
-  useEffect(() => {
-    if (token) {
-      const user = getUserByJWT(token)
-      dispatch(setToken(token))
-      dispatch(setUser(user))
-    }
-  }, [token, dispatch])
-
-  if (isError) {
-    if ('data' in error) {
-      return (
-        <div className="h-screen">
-          <Error status={error.status} />
-        </div>
-      )
-    }
-    return <div>Произошла ошибка соединения</div>;
-  }
-
   return (
     <>
-      {token && <AdminPanel />}
+      <AdminPanel />
       <ModalContainer />
     </>
   )
