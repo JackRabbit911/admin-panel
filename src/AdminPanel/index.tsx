1ef8e74e0927b978b9e8d9cc7ffb13766401bf2b
alx <world.zay@gmail.com>
2026-09-02 15:47:44 +0300

    #51 Move logic from App

--- src/AdminPanel/index.tsx
+++ src/AdminPanel/index.tsx
@@ -1,16 +1,24 @@
-import { useState } from "react"
 import { NavLink } from "react-router"
+import { useEffect, useState } from "react"
+
 import Navbar from "./Navbar"
 import Router from "./Router"
 import Sidebar from "./Sidebar"
 import Error from "Reused/Error"
-import { useAppDispatch, useAppSelector } from "shared/store/hooks"
+import { useGetQuery } from "shared/api"
+import { authUrl } from "shared/constants"
+import { getUserByJWT } from "shared/utils"
+import { setUser } from "shared/store/userSlice"
+import { setToken } from "shared/store/tokenSlice"
 import { resetStatus } from "shared/store/statusSlice"
+import { useAppDispatch, useAppSelector } from "shared/store/hooks"
 
 const AdminPanel = () => {
+  const { data } = useGetQuery(authUrl)
   const dispatch = useAppDispatch()
   const status = useAppSelector((state) => Number(state.status.status))
   const [isShown, setIsShown] = useState(false)
+  const token = data?.result ? data.result : null
 
   const onClose = () => {
     setIsShown(false)
@@ -20,8 +28,16 @@ const AdminPanel = () => {
     dispatch(resetStatus())
     setIsShown(false)
   }
+  
+  useEffect(() => {
+    if (token) {
+      const user = getUserByJWT(token)
+      dispatch(setToken(token))
+      dispatch(setUser(user))
+    }
+  }, [token])
 
-  return (
+  return token && (
     <div className="drawer md:drawer-open">
       <input
         id="my-drawer"
