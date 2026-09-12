3108fcef35d94db0f3a1da3eb0cd7f4d48e83ffb
alx <world.zay@gmail.com>
2026-09-12 14:08:43 +0300

    #55 Add titleStr and messageStr props

--- src/Reused/ConfirmModal.tsx
+++ src/Reused/ConfirmModal.tsx
@@ -1,7 +1,7 @@
 import { usePostMutation } from "shared/api"
 import { useTranslate } from "shared/i18n/hooks"
-import { actionResolve } from "Pages/Develop/Database/utils"
 import { openModalFn } from "./ModalContainer/utils"
+import { actionResolve } from "Pages/Develop/Database/utils"
 import type { ModalPropsMap } from "shared/store/modalSlice"
 
 type Props = {
@@ -13,6 +13,9 @@ const ConfirmModal = ({ props, onClose }: Props) => {
   const { title, message, url, method, messageSuccess } = actionResolve(props.actionType || 'dump')
   const [save] = usePostMutation()
   const __ = useTranslate()
+
+  const titleStr = props?.title || title
+  const messageStr = props?.message || message
 
   const handleConfirm = async () => {
       await save({
@@ -30,9 +33,9 @@ const ConfirmModal = ({ props, onClose }: Props) => {
     <>
       <div className="border p-4 rounded-sm flex flex-col gap-3 text-warning border-warning/20 bg-warning/5">
         <h3 className="text-lg font-bold flex items-center gap-2">
-          {__(title)}
+          {__(titleStr)}
         </h3>
-        <p className="text-sm text-base-content/80">{__(message)}</p>
+        <p className="text-sm text-base-content/80 wrap-break-word">{__(messageStr)}</p>
         <div className="modal-action mt-2 flex justify-center gap-4">
           <button className="btn btn-sm btn-outline" onClick={onClose}>
             {__(props.cancelText || 'Cancel')}
